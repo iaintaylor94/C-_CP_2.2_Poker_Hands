5fb42a1f9f2a9d4f96efc20a863536e45a2abe45
Iain Taylor <iain.taylor.94@gmail.com>
2024-05-20 09:41:40 +0000

    completed compareRanks()

--- PokerHands.cpp
+++ PokerHands.cpp
@@ -9,7 +9,13 @@ struct DeckRank PokerHands::getDeckRank (struct Hand *h) {
   
 }
 enum winner PokerHands::compareRanks (struct DeckRank *DRW, struct DeckRank *DRB) {
-  
+  if (DRW->rank > DRB->rank) return WHITE;
+  else if (DRW->rank < DRB->rank) return BLACK;
+  else {
+    if (DRW->highCard > DRB->highCard) return WHITE;
+    else if (DRW->highCard < DRB->highCard) return BLACK;
+    else return TIE;
+  }
 }
 
 
